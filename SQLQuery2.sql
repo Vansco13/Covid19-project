bef9f0dfdbb3e84f0779a50d8672dc5038c21099
Vansco13 <146702279+Vansco13@users.noreply.github.com>
2025-09-06 12:55:28 +0300

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -4,6 +4,7 @@
 --Select the data that I'll be using
 SELECT [location],[date],[total_cases],[new_cases],[total_deaths],[population]
 FROM[dbo].[CovidDeaths]
+WHERE [continent] IS NOT NULL
 ORDER BY [location],date
 
 --Looking at total cases vs total deaths in Diff countries
@@ -16,12 +17,53 @@ ORDER BY [location],date
 --Looking at the percentage of the population infected
 SELECT [location],[date],[total_cases],[population],ROUND(([total_cases]/population *100),2) AS 'Infection_rate'
 FROM[dbo].[CovidDeaths]
-WHERE [location]LIKE '%Kenya%'
+WHERE [continent] IS NOT NULL
 ORDER BY [location],date
 
 --What countries have the highest infection rates
 SELECT [location],MAX([total_cases]) AS 'HighestInfectionCount' ,[population],MAX(ROUND(([total_cases]/population *100),2)) AS 'Infection_rate'
 FROM[dbo].[CovidDeaths]
+WHERE [continent] IS NOT NULL
 GROUP BY [location],population
 ORDER BY Infection_rate DESC
+
+--Which countries had the most deaths
+SELECT [location],MAX(CAST([total_deaths] AS INT)) AS HighestDeathCount 
+FROM [dbo].[CovidDeaths]
+WHERE [continent] IS NOT NULL 
+GROUP BY [location]
+ORDER BY HighestDeathCount DESC
+
+--Wich continents had the most fatalities
+SELECT continent,MAX(CAST([total_deaths] AS INT)) AS HighestDeathCount 
+FROM [dbo].[CovidDeaths]
+WHERE [continent] IS NOT NULL 
+GROUP BY continent
+ORDER BY HighestDeathCount DESC
+
+--FINDING OUT THE GLOBAL NUMBERS OF FATALITIES AS OF THE MOST RECENT DATE
+SELECT [date],SUM([total_cases]) AS TotalCases,SUM(CAST([total_deaths] AS INT)) AS TotalDeaths,
+SUM(CAST([total_deaths] AS INT))/SUM([total_cases]) * 100 AS GlobalDeathRate
+FROM [dbo].[CovidDeaths]
+WHERE [continent] IS NOT NULL
+GROUP BY [date]
+ORDER BY [date] DESC
+
+
+ --Combining the deaths dataset with the vaccinations dataset
+ SELECT * 
+ FROM [dbo].[CovidDeaths]
+ JOIN[dbo].[CovidVaccinations]
+ ON[dbo].[CovidDeaths].location=[dbo].[CovidVaccinations].location
+ AND [dbo].[CovidDeaths].date=[dbo].[CovidVaccinations].date
+
+ --Let's look at the total population in the world that has been vaccinated
+ SELECT dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations
+ FROM [dbo].[CovidDeaths] dea
+ JOIN[dbo].[CovidVaccinations] vac
+ ON dea.location=vac.location
+ AND dea.date=vac.date
+ WHERE DEA.continent IS NOT NULL AND vac.new_vaccinations IS NOT NULL
+ ORDER BY 1,2,3
+
 
